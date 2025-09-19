860fc1c1d34a92e1a57d2104edd896fd631a19c5
Durdiyeva Rayxona <durdiyevarayxona60@gmail.com>
2025-09-19 17:51:44 +0500

    new file

---
+++ masala.py
@@ -0,0 +1,32 @@
+# son = int(input("Sonni kiriting:"))
+# if son > 0:
+#     print(f"{son} son musbat son")
+# elif son < 0:
+#     print(f"{son} son manfiy son")
+# else:
+#     print(f"Son 0 ga teng")
+###2
+# son1=int(input("Sonni kiriting:"))
+# son2=int(input("Sonni kiriting:"))
+#
+# if son1>son2:
+#     print(son1)
+# else:
+#     print(son2)
+
+###3
+# son1 = int(input("sonni kiriting"))
+# son2 = int(input("sonni kiriting"))
+# son3 = int(input("sonni kiriting"))
+# if son1 > son2:
+#     print(son1)
+# elif son2 > son3:
+#     print(son2)
+# else:
+#     print(f"son emas")
+###4
+# son= int(input("Sonni kiriting"))
+# if son % 2:
+#     print("son juft")
+# else:
+#     print("son toq")
